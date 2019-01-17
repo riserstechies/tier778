a170ef886709aba010f94b41010244a9f49b66f9
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-18 04:05:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/ECR4Kids-3-Tier-Metal-Rolling-Utility/dp/B075GSHSBN?ref_=Oct_DLandingS_PC_49b6bd09_NA&smid=ATVPDKIKX0DER">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/ECR4Kids-3-Tier-Metal-Rolling-Utility/dp/B075GSHSBN?ref_=Oct_DLandingS_PC_49b6bd09_NA&smid=ATVPDKIKX0DERs"
         </script>
         <title>Page Redirection</title>
     </head>
